23b245e08c5dfc4ab3d70b5c61a46ec8ba538146
Casey Zimmer <czimmer@ramapo.edu>
2018-09-14 03:01:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls
-  
-end
+    array.find? do |element|
+      element.length > 4
+    end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
