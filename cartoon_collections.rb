04feabb72b10a2576e1e680680b0772306749152
Casey Zimmer <czimmer@ramapo.edu>
2018-09-14 03:10:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,9 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
+  cheese_types_each
+  
   if(array.include?()
+    return "cheddar"
   
 end
