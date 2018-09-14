54cbf07ddb51c5710f40c4154bc9105d9615cf8a
Casey Zimmer <czimmer@ramapo.edu>
2018-09-14 02:58:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-    puts array.collect do |element|
+    array.collect do |element|
       element.capitalize
       element += "!"
     end
